5aeca03367dd64f00ba030701ee5d97fb35751aa
Ricardo Pereira <ricardo-d-pereira@telecom.pt>
2023-10-04 19:26:32 +0100

    static api json files

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const server = express();
 
+server.use('/api', express.static('db', {
+    extensions: ['json']
+}));
+
 server.get('*', (req, res) => res.send({
     now: Date.now(),
     foo: 'bar'
